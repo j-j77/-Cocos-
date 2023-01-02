7d57572d7c43024a24ccfe929622f8d6fa1cda66
李佳群 <121604282+j-j77@users.noreply.github.com>
2023-01-02 13:30:40 +0800

    Add files via upload

---
+++ resources/map/tileMap1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileMap1" tilewidth="40" tileheight="40" spacing="1" margin="1" tilecount="15" columns="5">
+ <image source="tileMap1.png" width="240" height="160"/>
+</tileset>
